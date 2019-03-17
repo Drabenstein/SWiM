484f61c7d4c34a7f4139fdc645951f98bb0c4c5a
Marcin Drabek <marcindrabek@hotmail.com>
2019-03-17 13:32:51 +0100

    Finished Zadanie1b

--- Zadanie1/app/src/main/java/net/mdrabek/zadanie1/MainActivity.java
+++ Zadanie1/app/src/main/java/net/mdrabek/zadanie1/MainActivity.java
@@ -1,10 +1,19 @@
 package net.mdrabek.zadanie1;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.support.v4.widget.ImageViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,20 +22,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Intent secondActivityIntent = new Intent(this, SecondActivity.class);
-        Button btn1 = (Button) findViewById(R.id.button1);
+        Button btn1 = (Button) findViewById(R.id.activity2btn);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(secondActivityIntent);
             }
         });
+
+        Switch langSwitch = findViewById(R.id.languageSwitch);
+        langSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Button activity2btn = findViewById(R.id.activity2btn);
+                Button activity3btn = findViewById(R.id.activity3btn);
+
+                if(isChecked)
+                {
+                    activity2btn.setText(R.string.activity2en);
+                    activity3btn.setText(R.string.activity3en);
+                }
+                else
+                {
+                    activity2btn.setText(R.string.activity2pl);
+                    activity3btn.setText(R.string.activity3pl);
+                }
+            }
+        });
+
+        final ImageView imgView = findViewById(R.id.img);
+        imgView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if(imgView.getAlpha() < 1.0f)
+                {
+                    imgView.setAlpha(1.0f);
+                }
+                else
+                {
+                    imgView.setAlpha(0.3f);
+                }
+                return true;
+            }
+        });
     }
 
-    public void startSecondActivity(View view)
-    {
-        final Intent secondActivityIntent = new Intent(this, SecondActivity.class);
-        startActivity(secondActivityIntent);
-    }
+//    public void startSecondActivity(View view)
+//    {
+//        final Intent secondActivityIntent = new Intent(this, SecondActivity.class);
+//        startActivity(secondActivityIntent);
+//    }
 
     public void startThirdActivity(View view)
     {
--- Zadanie1/app/src/main/java/net/mdrabek/zadanie1/SecondActivity.java
+++ Zadanie1/app/src/main/java/net/mdrabek/zadanie1/SecondActivity.java
@@ -4,6 +4,12 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends Activity {
 
@@ -11,14 +17,51 @@ public class SecondActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        View secondActivityLayout = (View) findViewById(R.id.text);
-        secondActivityLayout.setOnLongClickListener(new View.OnLongClickListener()
+    }
+
+    public void SubmitButtonClicked(View view)
+    {
+        TextView resultView = findViewById(R.id.resultView);
+        EditText nameView = findViewById(R.id.nameEdit);
+        EditText surnameView = findViewById(R.id.surnameEdit);
+        RadioGroup sexRadioGroup = findViewById(R.id.radioSex);
+        RadioButton sexRadioBtn = findViewById(sexRadioGroup.getCheckedRadioButtonId());
+        CheckBox javaCheckbox = findViewById(R.id.javaCheckBox);
+        CheckBox kotlinCheckbox = findViewById(R.id.kotlinCheckBox);
+        CheckBox csharpCheckbox = findViewById(R.id.csharpCheckBox);
+
+        StringBuilder result = new StringBuilder();
+        result.append(nameView.getText());
+        result.append(" ");
+        result.append(surnameView.getText());
+        result.append("\n");
+        result.append(sexRadioBtn.getText());
+        result.append("\n");
+        result.append("Znane języki: \n");
+
+        if(javaCheckbox.isChecked())
         {
-            @Override
-            public boolean onLongClick(View v) {
-                finish();
-                return false;
-            }
-        });
+            result.append(javaCheckbox.getText());
+            result.append("\n");
+        }
+
+        if(kotlinCheckbox.isChecked())
+        {
+            result.append(kotlinCheckbox.getText());
+            result.append("\n");
+        }
+
+        if(csharpCheckbox.isChecked())
+        {
+            result.append(csharpCheckbox.getText());
+            result.append("\n");
+        }
+
+        resultView.setText(result.toString());
+    }
+
+    public void BackButtonClicked(View view)
+    {
+        onBackPressed();
     }
 }
