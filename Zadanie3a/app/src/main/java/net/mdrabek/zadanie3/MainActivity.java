822d747b952496118188b51da1c607b894e984c2
Marcin Drabek <marcindrabek@hotmail.com>
2019-04-27 19:42:45 +0200

    Implemented bottom navigation bar

--- Zadanie3a/app/src/main/java/net/mdrabek/zadanie3/MainActivity.java
+++ Zadanie3a/app/src/main/java/net/mdrabek/zadanie3/MainActivity.java
@@ -1,16 +1,39 @@
 package net.mdrabek.zadanie3;
 
 import android.content.Intent;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = item -> {
+                switch (item.getItemId()) {
+                    case R.id.navXmlMenu:
+                        findViewById(R.id.xmlBasedMenuButton).performClick();
+                        return true;
+                    case R.id.navJavaMenu:
+                        findViewById(R.id.javaBasedMenuButton).performClick();
+                        return true;
+                    case R.id.navContextMenu:
+                        findViewById(R.id.contextMenuButton).performClick();
+                        return true;
+                    case R.id.navCheckableContextMenu:
+                        findViewById(R.id.checkableContextMenuButton).performClick();
+                        return true;
+                }
+                return false;
+            };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = findViewById(R.id.bottomNavigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
     }
 
     public void onXMLBasedMenuButtonClick(View view)
