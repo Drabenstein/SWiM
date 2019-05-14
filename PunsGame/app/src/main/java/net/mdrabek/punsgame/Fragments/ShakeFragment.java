66ff00fa35785d5ed49a747023079af8b3e87ddd
Marcin Drabek <marcindrabek@hotmail.com>
2019-05-14 02:13:30 +0200

    Reimplemented MainActivity using ViewPager

---
+++ PunsGame/app/src/main/java/net/mdrabek/punsgame/Fragments/ShakeFragment.java
@@ -0,0 +1,52 @@
+package net.mdrabek.punsgame.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import net.mdrabek.punsgame.R;
+
+public class ShakeFragment extends Fragment
+{
+    public ShakeFragment()
+    {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View root = inflater.inflate(R.layout.fragment_shake, container, false);
+
+        ImageView shakeImageView = root.findViewById(R.id.shakeImageView);
+
+        Glide.with(this).load(R.drawable.shake_phone_image).into(shakeImageView);
+
+        return root;
+    }
+
+    @Override
+    public void onAttach(Context context)
+    {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach()
+    {
+        super.onDetach();
+    }
+}
--- PunsGame/app/src/main/java/net/mdrabek/punsgame/MainActivity.java
+++ PunsGame/app/src/main/java/net/mdrabek/punsgame/MainActivity.java
@@ -5,25 +5,38 @@ import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Display;
 import android.view.Surface;
 import android.view.View;
 import android.widget.Toast;
 
+import net.mdrabek.punsgame.Fragments.ShakeFragment;
 import net.mdrabek.punsgame.Sensors.ShakeDetector;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements ShakeDetector.ShakeListener
+public class MainActivity extends FragmentActivity implements ShakeDetector.ShakeListener,
+        TabRequestedListener
 {
+    private static final int SHAKE_TAB_POSITION = 0;
+    private static final int CATEGORIES_TAB_POSITION = 1;
     public static final int START_GAME_REQ = 31231;
 
     private SensorManager sensorManager;
     private Sensor linearAccelerometer;
     private ShakeDetector shakeDetector;
     private long questionRandomSeed;
+
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,7 +46,39 @@ public class MainActivity extends AppCompatActivity implements ShakeDetector.Sha
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         linearAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         shakeDetector = new ShakeDetector(this);
-        sensorManager.registerListener(shakeDetector, linearAccelerometer, SensorManager.SENSOR_DELAY_UI);
+        registerShakeDetector();
+
+        viewPager = findViewById(R.id.viewpager);
+        pagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager(), this);
+        ScreenSlidePagerAdapter screenSlidePagerAdapter = (ScreenSlidePagerAdapter)pagerAdapter;
+        screenSlidePagerAdapter.addTabFragment(new ShakeFragment());
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    private void registerShakeDetector()
+    {
+        if (linearAccelerometer != null)
+        {
+            if (sensorManager == null)
+            {
+                sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+            }
+
+            if (shakeDetector == null)
+            {
+                shakeDetector = new ShakeDetector(this);
+            }
+
+            sensorManager.registerListener(shakeDetector, linearAccelerometer, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    private void unregisterShakeDetector()
+    {
+        if (sensorManager != null)
+        {
+            sensorManager.unregisterListener(shakeDetector);
+        }
     }
 
     public void changeOrientation(View v)
@@ -50,13 +95,15 @@ public class MainActivity extends AppCompatActivity implements ShakeDetector.Sha
     }
 
     @Override
-    public void onResume() {
+    public void onResume()
+    {
         super.onResume();
         sensorManager.registerListener(shakeDetector, linearAccelerometer, SensorManager.SENSOR_DELAY_UI);
     }
 
     @Override
-    public void onPause() {
+    public void onPause()
+    {
         sensorManager.unregisterListener(shakeDetector);
         super.onPause();
     }
@@ -65,7 +112,7 @@ public class MainActivity extends AppCompatActivity implements ShakeDetector.Sha
     public void onShake(int shakeCount, float acceleration)
     {
         Toast.makeText(this, "SHAKE no. " + shakeCount + " - " + acceleration, Toast.LENGTH_SHORT).show();
-        if(shakeCount < 3)
+        if (shakeCount < 3)
         {
             questionRandomSeed *= (long) shakeCount * 1000;
         }
@@ -84,11 +131,79 @@ public class MainActivity extends AppCompatActivity implements ShakeDetector.Sha
         startActivity(gameIntent);
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == START_GAME_REQ) {
-            if (resultCode == RESULT_OK) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == START_GAME_REQ)
+        {
+            if (resultCode == RESULT_OK)
+            {
                 Toast.makeText(this, "KONIEC GRY", Toast.LENGTH_SHORT).show();
             }
         }
     }
+
+    @Override
+    public void onBackPressed()
+    {
+        if (viewPager.getCurrentItem() == 0)
+        {
+            super.onBackPressed();
+        }
+        else
+        {
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
+    }
+
+    @Override
+    public void onTabRequested(int position)
+    {
+        if(position == SHAKE_TAB_POSITION)
+        {
+            registerShakeDetector();
+        }
+        else
+        {
+            unregisterShakeDetector();
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter
+    {
+        private List<Fragment> fragments;
+        private TabRequestedListener listener;
+
+        public ScreenSlidePagerAdapter(FragmentManager fm, TabRequestedListener listener) {
+            super(fm);
+            fragments = new ArrayList<>();
+            this.listener = listener;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            position %= fragments.size();
+
+            if(listener != null)
+            {
+                listener.onTabRequested(position);
+            }
+
+            return fragments.get(position);
+        }
+
+        public void addTabFragment(@NonNull Fragment fragment)
+        {
+            fragments.add(fragment);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+    }
+}
+
+interface TabRequestedListener
+{
+    void onTabRequested(int position);
 }
